6ac874816776a9a04c73823ac31e0a7b9aa36dd0
saiprasanthtav <saiprasanthtav@gmail.com>
2025-09-16 14:42:26 +0530

    modified index.js modified public/events.html

--- index.js
+++ index.js
@@ -49,9 +49,10 @@ const wrapHandler = (handler) => async (req, res) => {
 };
 
 app.all('/api/register', wrapHandler(require('./api/register')));
-app.all('/api/upi-qr', wrapHandler(require('./api/upi-qr.js')));
-app.all('/api/upi-config', wrapHandler(require('./api/upi-config.js')));
-app.all('/api/validate-utr', wrapHandler(require('./api/validate-utr.js')));
+// Removed: OTP and email endpoints
+// app.all('/api/upi-qr', wrapHandler(require('./api/upi-qr')));
+// app.all('/api/upi-config', wrapHandler(require('./api/upi-config')));
+// app.all('/api/validate-utr', wrapHandler(require('./api/validate-utr')));
 app.all('/api/live-excel', wrapHandler(require('./api/live-excel')));
 app.all('/api/download-excel', wrapHandler(require('./api/download-excel.js')));
 app.all('/api/registrations/:utr', wrapHandler(require('./api/registrations/[utr]')));
